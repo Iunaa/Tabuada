c5a3ab348ae833950ceb0684d6b718baeb48e634
iuna Leon <yuna.ads@hotmail.com>
2023-04-10 14:51:30 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+/*function contar(){
+    let msg = window.document.getElementById('msg')
+    let img = window.document.getElementById('imagem')
+    let data = new Date()
+    let hora = data.getHours()
+   
+
+}*/
+
